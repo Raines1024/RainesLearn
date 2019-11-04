51beca125117f86f0a33bc53a3c1f668ca99fc83
raines <17192192678@163.com>
2019-11-04 16:11:45 +0800

    ii

---
+++ src/main/java/com/raines/raineslearn/RainesLearnApplication.java
@@ -0,0 +1,13 @@
+package com.raines.raineslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainesLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainesLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raines/raineslearn/RainesLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raines.raineslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainesLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
