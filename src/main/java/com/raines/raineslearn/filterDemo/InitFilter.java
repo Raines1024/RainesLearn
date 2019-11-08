37169c1990457bab73fb03ddc7cc98d18459821d
raines <17192192678@163.com>
2019-11-08 13:43:58 +0800

    过滤器

--- src/main/java/com/raines/raineslearn/filterDemo/InitFilter.java
+++ src/main/java/com/raines/raineslearn/filterDemo/InitFilter.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -23,15 +24,15 @@ public class InitFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		//若当前session中存在账号，则将账号对应的操作机构也保存至session,方便以后查询
 		HttpServletRequest req = (HttpServletRequest)request;
 		if("user" == null) {
 			log.debug("用户未登录!!!!!!");
-			//response.sendRedirect("login");//未登录，可以重定向到登录页面
 			chain.doFilter(req, response);
 			return;
 		}
+
 		String uri = req.getRequestURI();
+		System.out.println(uri);
 		for(String subfix : excludes) {
 			if(uri.equalsIgnoreCase(subfix)) {
 				chain.doFilter(req, response);
---
+++ src/main/java/com/raines/raineslearn/filterDemo/demo/TbAssessController.java
@@ -0,0 +1,23 @@
+package com.raines.raineslearn.filterDemo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class TbAssessController {
+
+
+    @GetMapping("/create")
+    public boolean create() {
+        return true;
+    }
+
+
+    @GetMapping("/login")
+    public boolean login() {
+        return false;
+    }
+
+}
