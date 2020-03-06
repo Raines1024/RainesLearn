7ccf55eec8632d1d8ed0af37ba5fc5c7302dbccd
刘晓龙 <17192192678@163.com>
2020-03-06 14:43:37 +0800

    i

---
+++ src/main/java/com/raines/raineslearn/interesting/print/PrintSetMethod.java
@@ -0,0 +1,41 @@
+package com.raines.raineslearn.interesting.print;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 用处：
+ * 1.由实体类A向实体类B迁移数据
+ * 2.生成实体类A的所有set方法供使用（现在只能生成前缀为private String 的字段），根据需要自行扩展
+ */
+public class PrintSetMethod {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuffer stringBuffer = new StringBuffer();
+        List<String> resultList = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+            stringBuffer.append(s);
+            if (s.equals("exit")) {
+                break;
+            }
+        }
+        Arrays.asList(stringBuffer.toString().split("private String ")).forEach(x -> {
+            resultList.add(x.split(";")[0]);
+        });
+        resultList.forEach(x -> {
+            if (!x.trim().equals("")) {
+                String str = x.trim().substring(0, 1).toUpperCase() + x.substring(1);
+//                String get = "erpBaseCustomer.get" + str+"()";
+//                System.out.println("if("+get+"!=null){");
+                System.out.println("tFenceWarn.set" + str + "(" + ");");
+//                System.out.println("}");
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/raines/raineslearn/interesting/smallThing/TodayWeek.java
@@ -0,0 +1,35 @@
+package com.raines.raineslearn.interesting.smallThing;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Java获取今天周几
+ */
+public class TodayWeek {
+
+    public static void main(String[] args) {
+        String week = "";
+        Date today = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(today);
+        int weekday = c.get(Calendar.DAY_OF_WEEK);
+        if (weekday == 1) {
+            week = "周日";
+        } else if (weekday == 2) {
+            week = "周一";
+        } else if (weekday == 3) {
+            week = "周二";
+        } else if (weekday == 4) {
+            week = "周三";
+        } else if (weekday == 5) {
+            week = "周四";
+        } else if (weekday == 6) {
+            week = "周五";
+        } else if (weekday == 7) {
+            week = "周六";
+        }
+        System.out.println(week);
+    }
+
+}
